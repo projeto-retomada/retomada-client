e2d7a996b82738e2f4ba9dbb80d1e2eba1076e8e
mirielesilverio <mirielesilverio@hotmail.com>
2020-11-11 00:31:21 -0300

    Começando a estilizar template

--- src/containers/TheSidebar.vue
+++ src/containers/TheSidebar.vue
@@ -6,20 +6,8 @@
     @update:show="(value) => $store.commit('set', ['sidebarShow', value])"
   >
     <CSidebarBrand class="d-md-down-none" to="/">
-      <CIcon 
-        class="c-sidebar-brand-full" 
-        name="logo" 
-        size="custom-size" 
-        :height="35" 
-        viewBox="0 0 556 134"
-      />
-      <CIcon 
-        class="c-sidebar-brand-minimized" 
-        name="logo" 
-        size="custom-size" 
-        :height="35" 
-        viewBox="0 0 110 134"
-      />
+      <img src="img/logo.png" class="c-sidebar-brand-full" size="custom-size" height="35px" />
+      <img src="img/logo.png" class="c-sidebar-brand-minimized" size="custom-size" height="35px" />
     </CSidebarBrand>
 
     <CRenderFunction flat :content-to-render="$options.nav"/>
