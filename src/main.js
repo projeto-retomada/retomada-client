32d9a7c29656726841510031c52dd22533ba59fb
mirielesilverio <mirielesilverio@hotmail.com>
2020-11-24 22:32:30 -0300

    Instalando axios

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import router from './router'
 import CoreuiVue from '@coreui/vue'
 import { iconsSet as icons } from './assets/icons/icons.js'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCalendarCheck, faInfoCircle, faEllipsisH, faMapMarker } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -14,7 +16,9 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.performance = true
 Vue.use(CoreuiVue)
+Vue.use(VueAxios, axios)
 Vue.prototype.$log = console.log.bind(console)
+Vue.prototype.$RETOMADA = {}
 
 new Vue({
   el: '#app',
